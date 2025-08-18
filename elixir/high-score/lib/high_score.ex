a640611c56acb05baab6b3aec66cbfa3d179ca2d
Adam Hellberg <sharparam@sharparam.com>
2025-08-18 03:02:11 +0200

    Improve high score update
    Signed-off-by: Adam Hellberg <sharparam@sharparam.com>
    Change-Id: I6a6a69641339f0333a449a20fcb7340b94048424

--- elixir/high-score/lib/high_score.ex
+++ elixir/high-score/lib/high_score.ex
@@ -9,7 +9,7 @@ defmodule HighScore do
 
   def reset_score(scores, name), do: Map.put(scores, name, @initial_score)
 
-  def update_score(scores, name, score), do: Map.put(scores, name, score + Map.get(scores, name, 0))
+  def update_score(scores, name, score), do: Map.update(scores, name, score, fn value -> score + value end)
 
   def get_players(scores), do: Map.keys(scores)
 end
